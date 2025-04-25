b3b19bb1f80210907e7751125dede4a5bfc72474
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-04-25 15:10:42 -0300

    Algumas coisas

---
+++ src/@types/types.ts
@@ -0,0 +1,22 @@
+export interface CityResponse {
+  Version: number;
+  Key: string;
+  Type: string;
+  Rank: number;
+  LocalizedName: string;
+  Country: {
+    ID: string;
+    LocalizedName: string;
+  };
+  AdministrativeArea: {
+    ID: string;
+    LocalizedName: string;
+  };
+}
+
+export interface City {
+  id: string;
+  nome: string;
+  state?: string;
+  country: string;
+}
