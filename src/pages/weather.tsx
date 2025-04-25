b7f3a580406a75a2f9aec009b0a8863bdd6a35c5
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-04-25 16:26:44 -0300

    feat: Consumindo dados da API

--- src/pages/weather.tsx
+++ src/pages/weather.tsx
@@ -1,15 +1,33 @@
 import { useLocation } from "react-router";
 import { CityResponse } from "../@types/types";
+import { Card, Grid, Stack } from "@mui/material";
+import { useEffect } from "react";
+import { getCurrentConditions, getFiveDailyForecast } from "../api/fetch";
 
 export function Weather() {
   const location = useLocation();
   const data: CityResponse = location.state;
 
+  useEffect(() => {
+    async function fetchData() {
+      const conditions = await getCurrentConditions(data.Key);
+      const forecast = await getFiveDailyForecast(data.Key);
+
+      console.log(conditions);
+      console.log(forecast);
+    }
+    fetchData();
+  }, [data]);
+
   return (
-    <div>
-      <h1>Weather Page</h1>
-      <p>{data.LocalizedName}</p>
-      <p>{data.AdministrativeArea.LocalizedName}</p>
-    </div>
+    <Grid>
+      <Grid>
+        <Card></Card>
+      </Grid>
+      <Stack>
+        <Card></Card>
+        <Card></Card>
+      </Stack>
+    </Grid>
   );
 }
