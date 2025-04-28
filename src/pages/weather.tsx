94fa00dce16ca7dc5dd9664e718c6b116023d4fb
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-04-28 11:29:05 -0300

    feat: Adicionando os cars criados

--- src/pages/weather.tsx
+++ src/pages/weather.tsx
@@ -1,33 +1,69 @@
 import { useLocation } from "react-router";
-import { CityResponse } from "../@types/types";
-import { Card, Grid, Stack } from "@mui/material";
-import { useEffect } from "react";
+import { CityResponse, ConditionsProps } from "../@types/types";
+import { Box, Card, Grid, Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import { getCurrentConditions, getFiveDailyForecast } from "../api/fetch";
+import { formatExtensionDate, formatTime } from "../lib/utils";
+import { theme } from "../theme/global";
+import { CardDetails } from "../components/card-details";
+import { CardForecast } from "../components/card-forecast";
 
 export function Weather() {
   const location = useLocation();
   const data: CityResponse = location.state;
 
+  const [conditions, setConditions] = useState<ConditionsProps>();
+  //const [forecast, setForecast] = useState<any>();
+
   useEffect(() => {
     async function fetchData() {
-      const conditions = await getCurrentConditions(data.Key);
-      const forecast = await getFiveDailyForecast(data.Key);
+      const conditionsResponse = await getCurrentConditions(data.Key);
 
-      console.log(conditions);
-      console.log(forecast);
+      console.log(conditionsResponse);
+      //const forecast = await getFiveDailyForecast(data.Key);
+
+      setConditions(conditionsResponse[0]);
+      //setForecast(forecast.DailyForecasts);
     }
     fetchData();
   }, [data]);
 
+  if (!conditions) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <Grid>
-      <Grid>
-        <Card></Card>
-      </Grid>
+    <Stack direction="row" gap={2} sx={{ padding: "24px" }}>
       <Stack>
-        <Card></Card>
-        <Card></Card>
+        <Card
+          sx={{
+            backgroundColor: theme.palette.gray["gray-800"],
+            width: "814px",
+          }}
+        >
+          <Grid></Grid>
+          <Box>Buscar local</Box>
+          <Box>
+            <Stack>
+              <Grid>
+                <Stack>
+                  <Typography></Typography>
+                  <Typography>
+                    {formatExtensionDate(conditions.LocalObservationDateTime)}
+                  </Typography>
+                </Stack>
+                <Typography>
+                  {formatTime(conditions.LocalObservationDateTime)}
+                </Typography>
+              </Grid>
+            </Stack>
+          </Box>
+        </Card>
       </Stack>
-    </Grid>
+      <Stack gap={2}>
+        <CardDetails />
+        <CardForecast />
+      </Stack>
+    </Stack>
   );
 }
