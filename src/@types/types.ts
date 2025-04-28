ed0063cc8ca6cf3acea7fd863fbc355bdbf3e7a8
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-04-28 11:28:34 -0300

    feat: Novas tipagens

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -20,3 +20,117 @@ export interface City {
   state?: string;
   country: string;
 }
+
+interface ComponentProps {
+  Imperial: {
+    Value: number;
+    Unit: string;
+    UnitType: number;
+    Phrase?: string;
+  }
+  Metric: {
+    Value: number;
+    Unit: string;
+    UnitType: number;
+    Phrase?: string;
+  }
+}
+export interface ConditionsResponse {
+  ApparentTemperature: ComponentProps
+  Ceiling: ComponentProps
+  CloudCover: number
+  DewPoint: ComponentProps
+  EpochTime?: number
+  HasPrecipitation: boolean
+  IndoorRelativeHumidity: number
+  IsDayTime: boolean
+  Link: string
+  LocalObservationDateTime: string
+  MobileLink: string
+  ObstructionsToVisibility?: string
+  Past24HourTemperatureDeparture: ComponentProps
+  Precip1hr: ComponentProps
+  PrecipitationSummary: {
+    Past3Hours: ComponentProps
+    Past6Hours: ComponentProps
+    Past9Hours: ComponentProps
+    Past12Hours: ComponentProps
+    Past18Hours: ComponentProps
+    Past24Hours: ComponentProps
+    PastHour: ComponentProps
+    Precipitation: ComponentProps
+  }
+  PrecipitationType?: string
+  Pressure: ComponentProps
+  PressureTendency: ComponentProps 
+  RealFeelTemperature: ComponentProps
+  RealFeelTemperatureShade: ComponentProps
+  RelativeHumidity: number
+  Temperature: ComponentProps
+  TemperatureSummary: {
+    Past6Hours: {
+      Minimum: ComponentProps
+      Maximum: ComponentProps
+    }
+    Past12Hours: {
+      Minimum: ComponentProps
+      Maximum: ComponentProps
+    }
+    Past24Hours: {
+      Minimum: ComponentProps
+      Maximum: ComponentProps
+    }
+  }
+  UVIndex: number
+  UVIndexText: string
+  Visibility: ComponentProps
+  WeatherIcon: number
+  WeatherText: string
+  WetBulbGlobeTemperature: ComponentProps
+  WetBulbTemperature: ComponentProps
+  Wind: {
+    Direction: {
+      Degrees: number
+      Localized: string
+      English: string
+    }
+    Speed: ComponentProps
+  }
+  WindChillTemperature: ComponentProps
+  WindGust: {
+    Speed: ComponentProps
+  }
+}
+
+export interface ConditionsProps {
+  ApparentTemperature: ComponentProps
+  HasPrecipitation: boolean;
+  IndoorRelativeHumidity: number;
+  IsDayTime: boolean;
+  LocalObservationDateTime: string;
+  PrecipitationSummary: {
+    Precipitation: ComponentProps
+  }
+  RealFeelTemperature: ComponentProps
+  RelativeHumidity: number
+  Temperature: ComponentProps
+  TemperatureSummary: {
+    Past6Hours: {
+      Minimum: ComponentProps
+      Maximum: ComponentProps
+    }
+  }
+  UVIndex: number;
+  UVIndexText: string
+  WeatherIcon: number;
+  WeatherText: string;
+  Wind: {
+    Direction: {
+      Degrees: number;
+      Localized: string;
+      English: string;
+    }
+    Speed: ComponentProps
+  }
+}
+
